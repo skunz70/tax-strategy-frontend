bdda1ff86eadb69f0b9330bb934b1bbfc3f9db59
skunz70 <skunz@valhallataxservice.com>
2025-07-14 14:07:57 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TaxStrategyCard from "./TaxStrategyCard";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Tax Planner ROI Demo</h1>
+      <TaxStrategyCard />
+    </div>
+  );
+}
+
+export default App;
