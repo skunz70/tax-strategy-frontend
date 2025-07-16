4d7e2c3c435b190c442ee2c27a1822e80b23207e
skunz70 <skunz@valhallataxservice.com>
2025-07-16 15:29:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import TaxStrategyCard from "./TaxStrategyCard";
 import MultiYearRothCard from "./components/MultiYearRothCard";
 import CapitalGainsCard from "./components/CapitalGainsCard";
 import ScheduleCCard from "./components/ScheduleCCard";
-import RentalAnalysisCard from "./components/RentalAnalysisCard";
-import RetirementCard from "./components/RetirementCard"; // ✅ Import new card
+import RentalCard from "./components/RentalCard";
+import YearEndPlanningCard from "./components/YearEndPlanningCard";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 p-4">
       <h1 className="text-2xl font-bold mb-4 text-center">Tax Planner ROI Demo</h1>
       <TaxStrategyCard />
-      <div className="mt-6">
-        <MultiYearRothCard />
-      </div>
-      <div className="mt-6">
-        <CapitalGainsCard />
-        <ScheduleCCard />
-        <RentalAnalysisCard />
-        <RetirementCard /> {/* ✅ Render new card */}
-      </div>
+      <div className="mt-6"><MultiYearRothCard /></div>
+      <div className="mt-6"><CapitalGainsCard /></div>
+      <div className="mt-6"><ScheduleCCard /></div>
+      <div className="mt-6"><RentalCard /></div>
+      <div className="mt-6"><YearEndPlanningCard /></div>
     </div>
   );
 }
